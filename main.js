12d0bbc0e239accc888c6684ee4bf243a9135161
thejyotipatel <jyotipatelfyit77100@gmail.com>
2022-09-13 19:24:35 +0530

    add styled

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
- 
+const menuBtn = document.querySelector('.menu-btn')
+const menuNev = document.querySelector('.mobile-nev')
+const menuNevLinkBtn = document.querySelector('.mobile-nav-link')
+
+const toggleMenu = (e) => {
+  console.log('click')
+  menuNev.style.display === 'block'
+    ? (menuNev.style.display = 'none')
+    : (menuNev.style.display = 'block')
+}
+
+menuBtn.addEventListener('click', toggleMenu)
+menuNevLinkBtn.addEventListener('click', (e) => {
+  console.log('link')
+})
