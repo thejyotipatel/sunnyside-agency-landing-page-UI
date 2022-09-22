bf5b973139b4d8cf8c8ae031a55752a58bfd18c6
thejyotipatel <jyotipatelfyit77100@gmail.com>
2022-09-22 21:11:09 +0530

    responsive mobile menu

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn')
-const menuNev = document.querySelector('.mobile-nev')
+const menuNev = document.querySelector('.mobile-nav-bar')
 const menuNevLinkBtn = document.querySelector('.mobile-nav-link')
 
 const toggleMenu = (e) => {
   console.log('click')
-  menuNev.style.display === 'block'
+  menuNev.style.display === 'flex'
     ? (menuNev.style.display = 'none')
-    : (menuNev.style.display = 'block')
+    : (menuNev.style.display = 'flex')
 }
 
 menuBtn.addEventListener('click', toggleMenu)
-menuNevLinkBtn.addEventListener('click', (e) => {
-  console.log('link')
-})
+// menuNevLinkBtn.addEventListener('click', (e) => {
+// console.log('link')
+// })
